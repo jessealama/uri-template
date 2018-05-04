03a4ff35f2b6f179ebe89b4d1c702fe7c49cfa49
Jesse Alama <jesse@lisp.sh>
2018-05-04 04:54:38 +0200

    Start matching templates.

---
+++ match.rkt
@@ -0,0 +1,50 @@
+#lang racket/base
+
+(define (match-template-with-assignment template uri pos assignment)
+  ((or/c string? template?)
+   string?
+   exact-positive-integer?
+   assignment?
+   . -> .
+   (or/c false/c assignment?))
+  (cond ((empty? template)
+         assignment)
+        ((string? (first template))
+         (define x (first template))
+         (define xs (rest template))
+         (cond ((string-prefix? (substring uri pos) x)
+                (match-template-with-assignment xs
+                                                uri
+                                                (+ pos (string-length x))
+                                                assignment))
+               (else
+                #f)))
+        ((empty? (rest template))
+         (define x (first template))
+         (define v (substring uri pos))
+         (extend-assignment assignment x v))
+        ((template? (second template))
+         (match-template-with-assignment (rest template)
+                                         uri
+                                         pos
+                                         assignment))
+        ((not (string-contains? (substring uri pos)
+                                (second template)))
+         #f)
+        (else
+         #f)))
+
+(define/contract (match-template template uri)
+  (string? string? . -> . (or/c assignment? false/c))
+  (define parsed (parse-template template))
+  )
+
+(module+ test
+  (check-true (assignment? (match-template "http://example.com/~{username}/"
+                                           "http://example.com/~bob/")))
+  (check-true (assignment? (match-template "http://example.com/~{username}"
+                                           "http://example.com/~bob")))
+  (check-false (assignment? (match-template "http://example.com/~{username}/"
+                                            "http://example.com/~bob")))
+  (check-true (assignment? (match-template "http://example.com/~{username}"
+                                           "http://example.com/~bob/"))))
